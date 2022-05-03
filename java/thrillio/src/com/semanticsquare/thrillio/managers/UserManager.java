2a0126a83ae3befea03da06023110ccb491d6388
sergiu.toader <sergiu.toader99@gmail.com>
2022-05-03 14:37:01 +0300

    Partially implemented database manipulation classes

---
+++ java/thrillio/src/com/semanticsquare/thrillio/managers/UserManager.java
@@ -0,0 +1,34 @@
+package com.semanticsquare.thrillio.managers;
+
+import com.semanticsquare.thrillio.entities.User;
+
+public class UserManager {
+
+	private static UserManager instance;
+
+	private UserManager() {
+	}
+
+	public static UserManager getInstance() {
+
+		if (instance == null) {
+			instance = new UserManager();
+		}
+
+		return instance;
+	}
+
+	public User createUser(long id, String email, String password, String firstName, String lastName, int gender,
+			String userType) {
+		User user = new User();
+		user.setId(id);
+		user.setEmail(email);
+		user.setPassword(password);
+		user.setFirstName(firstName);
+		user.setLastName(lastName);
+		user.setGender(gender);
+		user.setUserType(userType);
+
+		return user;
+	}
+}
